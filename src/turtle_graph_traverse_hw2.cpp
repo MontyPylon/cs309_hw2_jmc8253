a8abf50c3d7595598cbaac9a6f7c11e50cc6ca53
Jeremy Cook <jeremycook@utexas.edu>
2017-02-17 09:12:24 -0600

    New algorithm that checks for the shortest distance for the turtle to travel. Murders other turtles once a sale is complete. Added launch file.
    Signed-off-by: Jeremy Cook <jeremycook@utexas.edu>

--- src/turtle_graph_traverse_hw2.cpp
+++ src/turtle_graph_traverse_hw2.cpp
@@ -3,8 +3,11 @@
 #include "geometry_msgs/Twist.h"
 #include "turtlesim/Pose.h"
 #include <cs309_turtlesim/turtle_constants.h>
+#include <turtlesim/Kill.h>
+#include <algorithm>
 #include <vector>
 #include <cmath>
+
 using namespace std;
 
 //global publisher for velocity
@@ -12,37 +15,35 @@ ros::Publisher velocity_pub;
 
 //the turtle's current pose gets stored here
 turtlesim::Pose current_pose;
-vector<double> remaining_x;
-vector<double> remaining_y;
 vector<double> next_target;
 bool at_target = true;
 bool first_msg = false;
-bool completedAngle = false;
-bool finished = false;
 double turningConstant = PI / 4;
 
-double linear_vel = 1;
-double angular_vel = 0.5;
+double linear_vel = MAX_LINEAR_VELOCITY;
+double angular_vel = MAX_ANGULAR_VELOCITY;
 
 // Stolen from jivko
 double euc_distance(double x1, double y1, double x2, double y2){
     return sqrt( (x1- x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) );    
 }
 
-bool travel_to_target(ros::Publisher velocity_pub, turtlesim::Pose pos, vector<double> next, bool angle) {
+bool travel_to_target(ros::Publisher velocity_pub, turtlesim::Pose pos, vector<double> next) {
     geometry_msgs::Twist msg;
 
     double d = euc_distance(pos.x, pos.y, next.at(0), next.at(1));
-    cout << "x: " << pos.x << ", y: " << pos.y << ", next_x " << next.at(0) << ", next_y: " << next.at(1) << endl;
-    cout << "Distance to next: " << d << endl;
+    //cout << "x: " << pos.x << ", y: " << pos.y << ", next_x " << next.at(0) << ", next_y: " << next.at(1) << endl;
+    //cout << "Distance to next: " << d << endl;
     if(d < DISTANCE_THRESHOLD) {
         // We are within the threshold
         cout << "Within threshold, moving to next turtle" << endl;
         msg.linear.x = 0.0;
+        msg.angular.z = 0.0;
+        velocity_pub.publish(msg);
         return true;
     } else {
         double ratio = (next.at(1) - pos.y) / (next.at(0) - pos.x);
-        cout << "CurY: " << pos.y << ", TarY: " << next.at(1) << ", CurX: " << pos.x << ", TarX: " << next.at(0) << endl;
+        //cout << "CurY: " << pos.y << ", TarY: " << next.at(1) << ", CurX: " << pos.x << ", TarX: " << next.at(0) << endl;
         double targetTheta = atan(ratio);
         if((next.at(0) - pos.x) < 0) {
             targetTheta += PI;
@@ -54,18 +55,17 @@ bool travel_to_target(ros::Publisher velocity_pub, turtlesim::Pose pos, vector<d
         if(pos.theta < 0) {
             currentAngle += 2*PI;
         }
-
-        cout << "currentAngle: " << currentAngle << ", targetTheta: " << targetTheta << endl;
-
-        if((targetTheta - currentAngle) > PI || (targetTheta - currentAngle) < 0) {
-            // Turn right
+        //cout << "currentAngle: " << currentAngle << ", targetTheta: " << targetTheta << endl;
+        
+        double deltaAngle = atan2(sin(targetTheta - currentAngle), cos(targetTheta - currentAngle));
+        
+        if (deltaAngle < 0) {
             msg.angular.z = -angular_vel;
         } else {
-            // Turn left
             msg.angular.z = angular_vel;
         }
 
-        if(abs(targetTheta - currentAngle) < turningConstant) {
+        if(abs(deltaAngle) < turningConstant) {
             msg.linear.x = linear_vel;
         }
 
@@ -74,34 +74,59 @@ bool travel_to_target(ros::Publisher velocity_pub, turtlesim::Pose pos, vector<d
     }
 }
 
-vector<double> find_nearest_neighbor(turtlesim::Pose pos, vector<double> rem_x, vector<double> rem_y) {
-    vector<double> returner;
+double find_distance(double x1, double y1, double x2, double y2) {
+    double dx = x1 - x2;
+    double dy = y1 - y2;
+    double dist = sqrt(pow(dx, 2) + pow(dy, 2));
+    return dist;
+}
 
-    double shortest = -1;
-    double shortest_x = -1;
-    double shortest_y = -1;
-    double list_num = -1;
-    for(int a = 0; a < rem_x.size(); a++) {
-        double dist_x = pos.x - rem_x.at(a);
-        double dist_y = pos.y - rem_y.at(a);
-        double pyth = sqrt(pow(dist_x, 2) + pow(dist_y, 2));
-        if(pyth < shortest || shortest == -1) {
-            shortest = pyth;
-            shortest_x = rem_x.at(a);
-            shortest_y = rem_y.at(a);
-            list_num = a;
+double find_total_distance(turtlesim::Pose pos, int order[], int size) {
+    double totalDist = 0;
+    for(int a = 0; a < size; a++) {
+        if(a == 0) {
+            totalDist = find_distance(pos.x, pos.y, x_coords[order[a]], y_coords[order[a]]);
+        } else {
+            totalDist += find_distance(x_coords[order[a-1]], y_coords[order[a-1]], x_coords[order[a]], y_coords[order[a]]);
         }
     }
+    return totalDist;
+}
 
-    if(shortest == -1) {
-        cout << "Finished" << endl;
-        finished = true;
+vector<int> find_basic_order(turtlesim::Pose pos) {
+    double shortest_dist = -1;
+    int size = (sizeof(x_coords)/sizeof(*x_coords));
+    int order[size];
+    vector<int> best_order;
+    
+    for(int a = 0; a < size; a++) {
+        order[a] = a;
+        best_order.push_back(a);
     }
+    
+    do {
+        double total_dist = find_total_distance(pos, order, size);
+        if(total_dist < shortest_dist || shortest_dist == -1) {
+            shortest_dist = total_dist;
+            for(int a = 0; a < size; a++) {
+                best_order[a] = order[a];
+            }
+        }
+    } while ( std::next_permutation(order,order+size) );
 
-    returner.push_back(shortest_x);
-    returner.push_back(shortest_y);
-    returner.push_back(list_num);
-    return returner;   
+    cout << "Shortest distance: " << shortest_dist << endl;
+    cout << "The order is: ";
+    for(int a = 0; a < size; a++) {
+        if(a != (size - 1)) {
+            cout << best_order.at(a) << ", ";
+        } else {
+            cout << best_order.at(a) << endl;
+        }
+    }
+    
+    
+    
+    return best_order;
 }
 
 void pose_cb(const turtlesim::Pose::ConstPtr& msg){
@@ -120,52 +145,64 @@ int main(int argc, char **argv){
     velocity_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
     //subscriber for the turtle's pose
     ros::Subscriber sub_pose = n.subscribe("/turtle1/pose", 1, pose_cb );
-    ros::Rate r(100);
+    ros::Rate r(60);
 
-    for(int a = 0; a < (sizeof(x_coords)/sizeof(*x_coords)); a++) {
-        remaining_x.push_back(x_coords[a]);
-        remaining_y.push_back(y_coords[a]);
-    }
-
+    ros::service::waitForService("kill");
+    ros::ServiceClient kill_client = n.serviceClient<turtlesim::Kill>("kill");
+    
+    bool calculate = true;
+    bool finished = false;
+    int solution = 0;
+    vector<int> best_order;
+    
     while(ros::ok()) {
-		
+
         //cout << "Current x: " << current_pose.x << endl;
         //cout << "Current y: " << current_pose.y << endl;
-
-        // Wait until we have the first message of the turtle's position
-        if(first_msg) {
-            //cout << current_pose.x << endl;
-            if(at_target && !finished) {
-                at_target = false;
-
-                // Next x and y position
-                next_target = find_nearest_neighbor(current_pose, remaining_x, remaining_y);
-
-                cout << "Next x pos: " << next_target.at(0) << endl;
-                cout << "Next y pos: " << next_target.at(1) << endl;
-                cout << "List num: " << next_target.at(2) << endl;
-
-                // Remove the target x and y from remaining set
-                remaining_x.erase(remaining_x.begin() + next_target.at(2));
-                remaining_y.erase(remaining_y.begin() + next_target.at(2));
-
-                if(!finished) {
-                    // Begin to travel to target position
-                    completedAngle = false;
-                    at_target = travel_to_target(velocity_pub, current_pose, next_target, completedAngle);
+        
+        if(first_msg && !finished) {
+            if(calculate) {
+                cout << "Calculating fastest route..." << endl;
+                best_order = find_basic_order(current_pose);
+                calculate = false;
+            } else {
+                
+                if(at_target) {
+                    
+                    if(solution != 0) {
+                        turtlesim::Kill srv;
+                        int numTurtle = best_order.at(solution - 1) + 2;
+                        std::stringstream strm;
+                        strm << "turtle" << numTurtle;
+                        srv.request.name = strm.str();
+                        kill_client.call(srv);
+                    }
+                    
+                    if(solution == best_order.size()) {
+                        cout << "We have finished!!" << endl;
+                        geometry_msgs::Twist msg;
+                        msg.linear.x = 0.0;
+                        msg.angular.z = 0.0;
+                        velocity_pub.publish(msg);
+                        finished = true;
+                    } else {
+                        if(next_target.empty()) {
+                            next_target.push_back(x_coords[best_order.at(solution)]);
+                            next_target.push_back(y_coords[best_order.at(solution)]);
+                        } else {
+                            next_target.at(0) = x_coords[best_order.at(solution)];
+                            next_target.at(1) = y_coords[best_order.at(solution)];
+                        }
+                        solution++;
+                        at_target = false;
+                        at_target = travel_to_target(velocity_pub, current_pose, next_target);
+                    }
                 } else {
-                    geometry_msgs::Twist msg;
-                    msg.linear.x = 0.0;
-                    msg.angular.z = 0.0;
-                    velocity_pub.publish(msg);
+                    at_target = travel_to_target(velocity_pub, current_pose, next_target);
                 }
-
-            } else if(!finished){
-                // at_target will be true when the turtle is at the position
-                at_target = travel_to_target(velocity_pub, current_pose, next_target, completedAngle);
             }
         }
-
+        
 		r.sleep();
 		ros::spinOnce();
 	}
